f70eb08fb0d8e7a5b494eee1e10c05a04c643cce
Adrian Sepulveda <153105681+AdrianSepulveda28@users.noreply.github.com>
2024-11-27 10:21:51 +0800

    Add files via upload

---
+++ src/main/java/com/ekphl/ekphl/ExcellentKnightPhApplication.java
@@ -0,0 +1,13 @@
+package com.ekphl.ekphl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcellentKnightPhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcellentKnightPhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekphl/ekphl/ExcellentKnightPhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekphl.ekphl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcellentKnightPhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
